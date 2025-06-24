26b89fc86189abbc6ed61d5e53d8a155c91fa539
Đurđina Filipović <djurdjinafilipovickg@gmail.com>
2025-06-24 19:19:12 +0200

    prvi commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b;
+    char c;
+    
+    cout << "Unesite brojeve i operaciju";
+    cin >> a >> b >> c;
+
+
+    return 0;
+}
